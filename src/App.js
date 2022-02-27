b5becd1896b9db2f3eeefa5487af612c16a2bfbe
bmong <tomo@hufs.ac.kr>
2022-02-27 23:53:31 +0900

    ✨캘린더 컴포넌트

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+import React from 'react';
+import { useState } from 'react';
+import DayPicker from 'react-day-picker';
+import DayPickerInput from 'react-day-picker/DayPickerInput'
+import 'react-day-picker/lib/style.css';
 import './App.css';
 
-function App() {
+import logo from './logo.svg';
+
+function OverlayComponent({ children, ...props }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div {...props}>
+      <p>My custom things</p>
+      
+      { children }
+      <p>Some content below the calendar</p>
     </div>
   );
 }
 
-export default App;
+function MyDayPicker() {
+  return <DayPicker overlayComponent={OverlayComponent} />;
+}
+
+export default function App() {
+  
+  const [day, setDay] = useState();
+
+  const handleDayClick = (day, { selected }) => {
+    if (selected) {
+      setDay(undefined);
+    }
+    else {
+      setDay(day);
+    }
+  }
+  
+  return (
+    <>
+    <div className="App">
+      <DayPicker 
+        onDayClick={handleDayClick} 
+        selectedDays={day}
+      />
+      {day ? <p>{day.toString()}</p> : <p>날짜를 선택해주세요.</p>}
+      <p>Please type a day:</p>
+      <DayPickerInput onDayChange={day => console.log(day)} />
+    </div>
+    </>
+  );
+}
